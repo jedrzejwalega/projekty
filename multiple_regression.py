fdd2bf8d640a788c927e5b92fb34f1e232fb66df
Jedrzej Walega <jedrzejwalega@gmail.com>
2019-08-30 03:43:34 +0200

    adding final regression and boston database exercises

--- multiple_regression.py
+++ multiple_regression.py
@@ -7,13 +7,15 @@ x2 = np.random.normal(43, 12, 100)
 # print(x1, "\n", "\n", x2)
 zipped = zip(x1, x2)
 # print(list(zipped))
+
+# Converting tuples into a list:
 x = []
 for x_1, x_2 in zipped:
     # print(x1, x2)
     x.append([x_1, x_2])
 # print(x)
 
-stutter = + np.random.normal(5, 3, 100)
+stutter = np.random.normal(5, 3, 100)
 y = x1 * 2 + stutter
 y.reshape(-1)
 
@@ -23,3 +25,15 @@ print(r_squared)
 b0 = model.intercept_
 b1 = model.coef_
 print(b0, "\n", b1)
+
+pred_x1 = np.random.normal(20, 3, 10)
+pred_x2 = np.random.normal(23, 3, 10)
+pred_x = zip(pred_x1, pred_x2)
+pred_x_lst = []
+for x1, x2 in pred_x:
+    # print(type(x1))
+    pred_x_lst.append([x1, x2])
+
+# print(pred_x_lst)
+pred_y = model.predict(pred_x_lst)
+print(pred_y)
