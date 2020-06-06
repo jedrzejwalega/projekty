d7d2076fbbdfa8896f74c55d31b208d78280df9a
Jedrzej Walega <jedrzejwalega@gmail.com>
2020-06-06 11:04:40 +0200

    sequential learning rate, color map joined plot

--- Other_projects/neural_net_final.py
+++ Other_projects/neural_net_final.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.filterwarnings('ignore',category=FutureWarning)
 import numpy as np
 import pandas as pd
 import csv
@@ -9,8 +11,15 @@ import matplotlib.pyplot as plt
 from torchvision.datasets import cifar
 from torchvision import transforms
 import timeit
-from math import ceil
 
+
+def adjust_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = next(learning_rates)
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
 
 def one_hot_encode(data: torch.utils.data.Dataset):
     return keras.utils.to_categorical(data, 10)
@@ -39,65 +48,77 @@ test_data = list(cifar.CIFAR10("/home/jedrzej/Desktop/Machine_learning/", downlo
 
 entry_len = training_data[0][0].shape[0]
 
-net = SimpleNet().to(device)
+learning_sets = [[8, 1, 0.5], [5, 1, 0.5], [2, 1, 0.5]]
 criterion = nn.MSELoss(reduction="mean")
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 batch_size = 128
-epochs = 50000
+epochs = 60
 
 train_loader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=50000, shuffle=False)
 
-start = timeit.default_timer()
-
-losses = []
-testing_losses = []
-for epoch in range(epochs):
-    running_loss_count = 0.0
-    running_loss_sum = 0.0
-    train_iter = iter(train_loader)
-    for x_batch, y_batch in train_iter:
-                x_batch, y_batch = x_batch.to(device), y_batch.to(device)
-                # forward pass
-                preds = net(x_batch)
-
-                # backward pass
-                loss = criterion(preds, y_batch)
-                optimizer.zero_grad()
-                loss.backward()
-
-                # update parameters
-                optimizer.step()
-
-                # print progress
-                running_loss_count += batch_size
-                running_loss_sum += loss.item() * batch_size
-    running_loss_mean = running_loss_sum/running_loss_count
-    losses.append(running_loss_mean)
-    print(f"In epoch {epoch}: Testing loss mean: {running_loss_mean}")
-
-    running_loss_test_count = 0.0
-    running_loss_test_sum = 0.0
-    test_iter = iter(test_loader)
-    with torch.no_grad():
-        for x_test, y_test in test_iter:
-            x_test, y_test = x_test.to(device), y_test.to(device)
-            predictions = net(x_test)
-            testing_loss = criterion(predictions, y_test)
-            running_loss_test_count += batch_size
-            running_loss_test_sum += testing_loss.item() * batch_size
-        running_loss_test_mean = running_loss_test_sum / running_loss_test_count
-        testing_losses.append(running_loss_test_mean)
-    print(f"In epoch {epoch}: Testing loss mean: {running_loss_test_mean}")
-
-print ("\n ### Finished Training ### \n")
-stop = timeit.default_timer()
-print(stop - start)
+color_map = plt.cm.get_cmap('rainbow', len(learning_sets))
+n=0
 
 figure, axes = plt.subplots(figsize=(12.8, 14.4))
-axes.plot(range(epochs), losses, color="red")
-axes.plot(range(epochs), testing_losses, color="blue")
 plt.title("Loss change in model training", fontsize=18)
 axes.set_xlabel("Epochs", fontsize=15)
 axes.set_ylabel("Loss", fontsize=15)
+
+
+start = timeit.default_timer()
+
+for rates in learning_sets:
+    learning_rates = iter(rates)
+    net = SimpleNet().to(device)
+    optimizer = optim.SGD(net.parameters(), lr=next(learning_rates), momentum=0.9)
+    losses = []
+    testing_losses = []
+    for epoch in range(epochs):
+        print(get_lr(optimizer))
+        running_loss_count = 0.0
+        running_loss_sum = 0.0
+        train_iter = iter(train_loader)
+        for x_batch, y_batch in train_iter:
+                    x_batch, y_batch = x_batch.to(device), y_batch.to(device)
+                    # forward pass
+                    preds = net(x_batch)
+
+                    # backward pass
+                    loss = criterion(preds, y_batch)
+                    optimizer.zero_grad()
+                    loss.backward()
+
+                    # update parameters
+                    optimizer.step()
+
+                    # print progress
+                    running_loss_count += batch_size
+                    running_loss_sum += loss.item() * batch_size
+        running_loss_mean = running_loss_sum/running_loss_count
+        losses.append(running_loss_mean)
+        print(f"In epoch {epoch}: Training loss mean: {running_loss_mean}")
+
+        if epoch % 20 == 0 and epoch > 0:
+            adjust_lr(optimizer)
+
+        running_loss_test_count = 0.0
+        running_loss_test_sum = 0.0
+        test_iter = iter(test_loader)
+        with torch.no_grad():
+            for x_test, y_test in test_iter:
+                x_test, y_test = x_test.to(device), y_test.to(device)
+                predictions = net(x_test)
+                testing_loss = criterion(predictions, y_test)
+                running_loss_test_count += batch_size
+                running_loss_test_sum += testing_loss.item() * batch_size
+            running_loss_test_mean = running_loss_test_sum / running_loss_test_count
+            testing_losses.append(running_loss_test_mean)
+        print(f"In epoch {epoch}: Testing loss mean: {running_loss_test_mean}")
+
+    stop = timeit.default_timer()
+    print (f"\n ### Finished Training in {stop - start} ### \n")
+    axes.plot(range(epochs), losses, color=color_map(n), label=f"Training lr = {rates}")
+    axes.plot(range(epochs), testing_losses, color=np.array(color_map(n)) * 0.6, label=f"Testing lr = {rates}")
+    n += 1
+axes.legend()
 plt.show()
