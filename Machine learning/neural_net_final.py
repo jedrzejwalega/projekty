87ee82b189e334dcb0f62a849062c187d74690ed
Jedrzej Walega <jedrzejwalega@gmail.com>
2020-07-15 22:05:24 +0200

    clean version with objects

--- Machine learning/neural_net_final.py
+++ Machine learning/neural_net_final.py
@@ -17,6 +17,92 @@ from itertools import cycle
 from math import ceil
 
 
+class Model():
+    def __init__(self, criterion, batch_size):
+        # Model parameters
+        self.net = SimpleNet()
+        self.optimizer = None
+        self.criterion = criterion
+        self.batch_size = batch_size
+        # Different losses containers
+        self.losses = []
+        self.testing_losses = []
+        self.min_training_loss = None
+        self.min_testing_loss = None
+        self.min_training_local_losses = []
+        self.min_testing_local_losses = []
+    
+    def train(self, train_loader):
+        running_loss_count = 0.0
+        running_loss_sum = 0.0
+        train_iter = iter(train_loader)
+        batch_number = 0
+        for x_batch, y_batch in train_iter:
+            batch_number += 1
+            x_batch, y_batch = x_batch.to(device), y_batch.to(device)
+            # forward pass
+            preds = self.net(x_batch)
+
+            # backward pass
+            loss = self.criterion(preds, y_batch)
+            self.optimizer.zero_grad()
+            loss.backward()
+
+            # update parameters
+            self.optimizer.step()
+
+            # print progress
+            running_loss_count += self.batch_size
+            running_loss_sum += loss.item() * self.batch_size
+
+            if batch_number % 20 == 0:
+                running_loss_mean = running_loss_sum/running_loss_count
+                self.losses.append(running_loss_mean)
+                running_loss_sum = 0.0
+                running_loss_count = 0.0
+                batch_number = 0
+        if batch_number != 0:
+            running_loss_mean = running_loss_sum/running_loss_count
+            self.losses.append(running_loss_mean)
+
+        return self.losses
+
+    def adjust_lr(self, new_lr):
+        for param_group in self.optimizer.param_groups:
+            param_group["lr"] = new_lr
+    
+    def test(self, test_loader):
+        running_loss_test_count = 0.0
+        running_loss_test_sum = 0.0
+        test_iter = iter(test_loader)
+        batch_number = 0
+        with torch.no_grad():
+            for x_test, y_test in test_iter:
+                batch_number += 1
+                x_test, y_test = x_test.to(device), y_test.to(device)
+                predictions = self.net(x_test)
+                testing_loss = self.criterion(predictions, y_test)
+                running_loss_test_count += self.batch_size
+                running_loss_test_sum += testing_loss.item() * self.batch_size
+                
+                if batch_number % 20 == 0:
+                    running_loss_test_mean = running_loss_test_sum/running_loss_test_count
+                    self.testing_losses.append(running_loss_test_mean)
+                    running_loss_test_sum = 0.0
+                    running_loss_test_count = 0.0
+                    batch_number = 0
+
+        if batch_number != 0:
+            running_loss_test_mean = running_loss_test_sum/running_loss_test_count
+            self.testing_losses.append(running_loss_test_mean)
+
+        return self.testing_losses
+        
+    def get_lr(self):
+        for param_group in self.optimizer.param_groups:
+            return param_group['lr']
+
+
 class SimpleNet(nn.Module):
     def __init__(self):
         super(SimpleNet, self).__init__()
@@ -29,15 +115,7 @@ class SimpleNet(nn.Module):
         x = torch.sigmoid(self.fc2(x))
         x = self.fc3(x)
         return x
-
-
-def adjust_lr(optimizer, new_lr):
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = new_lr
-
-def get_lr(optimizer):
-    for param_group in optimizer.param_groups:
-        return param_group['lr']
+    
 
 def one_hot_encode_digits(data: torch.utils.data.Dataset):
     return keras.utils.to_categorical(data, 10)
@@ -46,7 +124,6 @@ def flatten_vector(data: torch.utils.data.Dataset):
     return torch.flatten(data)
 
 def train_and_test_model(learning_sets:List[List[float]], batch_size:int, epochs_per_lr:List[List[float]], min_by_epochs:List[List[float]], path=str):
-    criterion = nn.MSELoss(reduction="mean")
     train_loader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
@@ -58,23 +135,24 @@ def train_and_test_model(learning_sets:List[List[float]], batch_size:int, epochs
 
     start = timeit.default_timer()
     
-    minimal_losses = []
     losses_per_epoch = ceil(len(train_loader.dataset)/batch_size/20)
     
+    deciding_losses = []
     for rates, epoch_limits, epochs_of_interest in zip(learning_sets, epochs_per_lr, min_by_epochs):
-        print(rates, epoch_limits, epochs_of_interest)
+        print(f"Learning rates: {rates}\nEpochs per lr: {epoch_limits}\nBest lr chosen by epochs: {epochs_of_interest}")
         torch.manual_seed(1)
         learning_rates = cycle(rates)
-        net = SimpleNet().to(device)
-        optimizer = optim.SGD(net.parameters(), lr=next(learning_rates), momentum=0.9)
-        losses = []
-        testing_losses = []
+        criterion = nn.MSELoss(reduction="mean")
+        model = Model(criterion=criterion, batch_size=batch_size)
+        model.optimizer = optim.SGD(model.net.parameters(), lr=next(learning_rates), momentum=0.9)
+        model.net = model.net.to(device)
+        
         for epochs in epoch_limits:
             for epoch in range(epochs):
-                losses = train(net, optimizer, train_loader, losses, epoch, criterion, batch_size)
-                testing_losses = test(net, test_loader, testing_losses, epoch, criterion, batch_size)
+                losses = model.train(train_loader)
+                testing_losses = model.test(test_loader)
             new_lr = next(learning_rates)
-            adjust_lr(optimizer, new_lr)
+            model.adjust_lr(new_lr)
 
         stop = timeit.default_timer()
         print (f"\n ### Finished Training in {stop - start} ### \n")
@@ -84,31 +162,34 @@ def train_and_test_model(learning_sets:List[List[float]], batch_size:int, epochs
         testing_losses_of_interest = [x for n in epochs_of_interest for x in testing_losses[(n-1) * losses_per_epoch:n * losses_per_epoch]]
         
         # Minimal losses only by chosen epochs of interest
-        min_training_loss_no_extra, min_testing_loss_no_extra = calculate_min_losses(losses_of_interest, testing_losses_of_interest)
-        minimal_losses.append((rates, min_training_loss_no_extra, min_testing_loss_no_extra))
+        deciding_min_training_loss, deciding_minimal_testing_loss = calculate_min_losses(losses_of_interest, testing_losses_of_interest)
+        deciding_losses.append((rates, deciding_min_training_loss, deciding_minimal_testing_loss))
         
+        # Minimal losses by all epochs
+        model.min_training_loss, model.min_testing_loss = calculate_min_losses(losses, testing_losses)
+
         # Minimal local losses, meaning minimal loss for every learning rate used
-        min_training_loss, min_testing_loss = calculate_min_losses(losses, testing_losses)
-        min_training_local_losses, min_testing_local_losses = calculate_local_losses(losses, testing_losses, epoch_limits)
+        model.min_training_local_losses, modelmin_testing_local_losses = calculate_local_losses(losses, testing_losses, epoch_limits)
         
         # Plot training and testing losses on one plot
         all_epochs = sum(epoch_limits)
-        plot_training_losses(axes_both, all_epochs, losses, color_map, color_map_index, min_training_loss, min_training_local_losses, rates)
-        plot_testing_losses(axes_both, all_epochs, testing_losses, color_map, color_map_index, min_testing_loss, min_testing_local_losses, rates)
+        color = color_map(color_map_index)
+        plot_training_losses(axes_both, all_epochs, model, color, rates)
+        plot_testing_losses(axes_both, all_epochs, model, color, rates)
         
         # Plot only training losses
-        plot_training_losses(axes_training, all_epochs, losses, color_map, color_map_index, min_training_loss, min_training_local_losses, rates)
+        plot_training_losses(axes_training, all_epochs, model, color, rates)
         
         # Plot only testing losses
-        plot_testing_losses(axes_testing, all_epochs, testing_losses, color_map, color_map_index, min_testing_loss, min_testing_local_losses, rates)
+        plot_testing_losses(axes_testing, all_epochs, model, color, rates)
         color_map_index += 1
     
     save_plot(figure_both, axes_both, path + "_both.png")
     save_plot(figure_training, axes_training, path + "_training.png")
     save_plot(figure_testing, axes_testing, path + "_testing.png")
     
-    write_to_csv(path, minimal_losses)
-    return min(minimal_losses, key=lambda x:x[1])
+    write_to_csv(path, deciding_losses)
+    return min(deciding_losses, key=lambda x:x[1])
 
 def set_up_plot(title, x_label_title, y_label_title):
     figure, axes = plt.subplots(figsize=(12.8, 14.4))
@@ -119,71 +200,6 @@ def set_up_plot(title, x_label_title, y_label_title):
     axes.grid(axis="both", which="both")
    
     return figure, axes
-
-def train(model, optimizer, train_loader, losses, epoch, criterion, batch_size):
-    # print(f"Epoch {epoch} training learning rate: ", get_lr(optimizer))
-    running_loss_count = 0.0
-    running_loss_sum = 0.0
-    train_iter = iter(train_loader)
-    batch_number = 0
-    for x_batch, y_batch in train_iter:
-        batch_number += 1
-        x_batch, y_batch = x_batch.to(device), y_batch.to(device)
-        # forward pass
-        preds = model(x_batch)
-
-        # backward pass
-        loss = criterion(preds, y_batch)
-        optimizer.zero_grad()
-        loss.backward()
-
-        # update parameters
-        optimizer.step()
-
-        # print progress
-        running_loss_count += batch_size
-        running_loss_sum += loss.item() * batch_size
-
-        if batch_number % 20 == 0:
-            running_loss_mean = running_loss_sum/running_loss_count
-            losses.append(running_loss_mean)
-            running_loss_sum = 0.0
-            running_loss_count = 0.0
-            batch_number = 0
-    if batch_number != 0:
-        running_loss_mean = running_loss_sum/running_loss_count
-        losses.append(running_loss_mean)
-
-    # print(f"Epoch {epoch}: Training loss mean: {running_loss_mean}")
-    return losses
-
-def test(model, test_loader, testing_losses, epoch, criterion, batch_size):
-    running_loss_test_count = 0.0
-    running_loss_test_sum = 0.0
-    test_iter = iter(test_loader)
-    batch_number = 0
-    with torch.no_grad():
-        for x_test, y_test in test_iter:
-            batch_number += 1
-            x_test, y_test = x_test.to(device), y_test.to(device)
-            predictions = model(x_test)
-            testing_loss = criterion(predictions, y_test)
-            running_loss_test_count += batch_size
-            running_loss_test_sum += testing_loss.item() * batch_size
-            
-            if batch_number % 20 == 0:
-                running_loss_test_mean = running_loss_test_sum/running_loss_test_count
-                testing_losses.append(running_loss_test_mean)
-                running_loss_test_sum = 0.0
-                running_loss_test_count = 0.0
-                batch_number = 0
-
-    if batch_number != 0:
-        running_loss_test_mean = running_loss_test_sum/running_loss_test_count
-        testing_losses.append(running_loss_test_mean)
-
-    # print(f"Epoch {epoch}: Testing loss mean: {running_loss_test_mean}")
-    return testing_losses
 
 def calculate_min_losses(losses, testing_losses):
     min_training_loss = min(losses)
@@ -201,11 +217,11 @@ def calculate_local_losses(losses, testing_losses, epochs):
 
     return min_training_local_losses, min_testing_local_losses
 
-def plot_training_losses(axes, epochs, losses, color_map, color_map_index, min_training_loss, min_training_local_losses, rates):
-    axes.plot(np.arange(0, epochs, 1/(len(losses)/epochs)), losses, color=color_map(color_map_index), label=f"Lr={rates}, Training min={min_training_loss}, Local mins={min_training_local_losses}")
+def plot_training_losses(axes, epochs, model, color, rates):
+    axes.plot(np.arange(0, epochs, 1/(len(model.losses)/epochs)), model.losses, color=color, label=f"Lr={rates}, Training min={model.min_training_loss}, Local mins={model.min_training_local_losses}")
 
-def plot_testing_losses(axes, epochs, testing_losses, color_map, color_map_index, min_testing_loss, min_testing_local_losses, rates):
-    axes.plot(np.arange(0, epochs, 1/(len(testing_losses)/epochs)) , testing_losses, color=np.array(color_map(color_map_index)) * 0.6, label=f"Lr={rates}, Testing min={min(testing_losses)}, Local mins={min_testing_local_losses}")
+def plot_testing_losses(axes, epochs, model, color, rates):
+    axes.plot(np.arange(0, epochs, 1/(len(model.testing_losses)/epochs)) , model.testing_losses, color=np.array(color) * 0.6, label=f"Lr={rates}, Testing min={model.min_testing_loss}, Local mins={model.min_testing_local_losses}")
 
 def save_plot(figure, axes, path):
     axes.legend(bbox_to_anchor=(1.04,1), loc="upper left")
